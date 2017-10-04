836ee07bb62992c070e89762cdd91aacce85ac4f
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2017-10-04 18:49:11 +0100

    Add dofs/process to x axis of matvec plots

--- results/plot-rb-matvecs.py
+++ results/plot-rb-matvecs.py
@@ -12,14 +12,14 @@ data = "rb-matvecs.csv"
 num_processes = 48
 
 if not os.path.exists(data):
-    print "Requested data file '%s' does not exist" % data
+    print("Requested data file '%s' does not exist" % data)
     sys.exit(1)
 
 
 dataframe = pandas.read_csv(data)
 
-dataframe["assemble_per_dof"] = dataframe.assemble_time/dataframe.rows
-dataframe["matvec_per_dof"] = dataframe.matmult_time/dataframe.rows
+dataframe["assemble_dof_per_second"] = (dataframe.rows/dataframe.assemble_time)
+dataframe["matvec_dof_per_second"] = (dataframe.rows/dataframe.matmult_time)
 dataframe["bytes_per_dof"] = dataframe.bytes/dataframe.rows
 
 seaborn.set(style="ticks")
@@ -27,12 +27,12 @@ seaborn.set(style="ticks")
 fig = pyplot.figure(figsize=(9, 5), frameon=False)
 ax = fig.add_subplot(111)
 
-ax.set_xlabel("Polynomial degree of scalar space", fontsize=FONTSIZE)
+ax.set_xlabel("Polynomial degree\n(2D dofs/process)\n(3D dofs/process)", fontsize=FONTSIZE)
 
-ax.set_ylabel("Time/dof [s]", fontsize=FONTSIZE)
+ax.set_ylabel("dofs/second", fontsize=FONTSIZE)
 
-ax.set_ylim([dataframe.matvec_per_dof.min()/1.5,
-             dataframe.assemble_per_dof.max()*1.5])
+ax.set_ylim([dataframe.assemble_dof_per_second.min()/1.5,
+             dataframe.matvec_dof_per_second.max()*1.5])
 ax.semilogy()
 
 ax.set_xticks([1, 2, 3, 4, 5])
@@ -54,7 +54,7 @@ for dim in dataframe["dimension"].drop_duplicates():
         dimstr = {2: "2D",
                   3: "3D"}[dim]
         if typ != "matfree":
-            ax.plot(sliced.degree, sliced.assemble_per_dof,
+            ax.plot(sliced.degree, sliced.assemble_dof_per_second,
                     label="Assemble %s [%s]" % (name, dimstr),
                     linewidth=LINEWIDTH, linestyle=linestyle,
                     markersize=MARKERSIZE,
@@ -62,13 +62,34 @@ for dim in dataframe["dimension"].drop_duplicates():
                     color=next(colors),
                     clip_on=False)
 
-        ax.plot(sliced.degree, sliced.matvec_per_dof,
+        ax.plot(sliced.degree, sliced.matvec_dof_per_second,
                 label="MatMult %s [%s]" % (name, dimstr),
                 linewidth=LINEWIDTH, linestyle=linestyle,
                 markersize=MARKERSIZE,
                 marker=next(markers),
                 color=next(colors),
                 clip_on=False)
+
+
+def doflabel(dofs):
+    if dofs > 1e6:
+        return '%.1fM' % (dofs/(1e6))
+    elif dofs > 1e3:
+        return '%dk' % (dofs/(1e3))
+    return '%d' % (dofs)
+
+
+sliced = dataframe.loc[lambda df: df.type == "aij"].loc[lambda df: df.num_processes == num_processes]
+
+sliced = sliced.groupby(["degree", "dimension"], as_index=False).mean()
+
+twod_dofs = ["(%s)" % doflabel(d/num_processes) for d in sliced.loc[lambda df: df.dimension == 2].rows]
+threed_dofs = ["(%s)" % doflabel(d/num_processes) for d in sliced.loc[lambda df: df.dimension == 3].rows]
+for _ in range(len(twod_dofs) - len(threed_dofs)):
+    threed_dofs.append("")
+
+ax.set_xticks(list(range(1, 1+len(twod_dofs))))
+ax.set_xticklabels(["%s\n%s\n%s" % (d+1, twod_dofs[d], threed_dofs[d]) for d in range(len(twod_dofs))])
 
 for tick in ax.get_xticklabels():
     tick.set_fontsize(FONTSIZE)
